6ea8621a890985b30327049a37b30561fb1a8692
Agata <agata.cuper888@onet.pl>
2020-05-28 19:04:16 +0200

    Extract App component

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 
 function Clock({ className = "", minutes = 20, seconds = 48 }) {
     return <h2 className={"Clock " + className}>Pozostało {minutes}:{seconds}</h2>
